950af37b2a647101529cde1a06101943284e07a0
Muskan kumari <muskanrazz006@gmail.com>
2026-05-06 22:58:25 +0530

    Time: 1 ms (99.83%), Space: 43.4 MB (8.43%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] arr1= s.toCharArray();
+        char[] arr2= t.toCharArray();
+    //   int n= s.length;
+    //   int m= t.length;
+      int xor1=0;
+
+      for(int i=0; i<arr1.length; i++){
+        xor1= xor1^ (int)arr1[i];
+      }
+      
+        for(int i=0; i<arr2.length; i++){
+            xor1= xor1 ^ arr2[i];
+        }
+      return (char)(xor1);
+
+    }
+}
