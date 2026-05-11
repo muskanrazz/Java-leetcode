9f4cf72d7f2bfc1fb847da545e75fa9fa4984b90
Muskan kumari <muskanrazz006@gmail.com>
2026-05-11 21:52:20 +0530

    Time: 1 ms (96.67%), Space: 42.1 MB (96.48%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        boolean check=true;
+        if(n<=0){
+            return false;
+        }
+        while(n!=1){
+            
+            if(n%2==0){
+              n=n/2;
+            }
+            else{
+                  return false;
+            }
+        }
+            return true;
+        
+    }
+}
