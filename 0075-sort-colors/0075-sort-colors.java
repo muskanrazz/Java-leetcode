2b3d65fe488a448a0c6ec78b12765aa304898381
Muskan kumari <muskanrazz006@gmail.com>
2026-05-14 23:41:09 +0530

    Time: 0 ms (100%), Space: 43.4 MB (76.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,31 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-      int n= nums.length;
-      int copy[]= new int[n];
+      int n =  nums.length;
+      int copy[]= new int [n];
       int j=0;
 
-      for(int i=0; i<n; i++){
-         if(nums[i]==0){
+     for(int i=0; i<n; i++){
+        if(nums[i]==0){
             copy[j]=nums[i];
             j++;
-         }
-      } 
-      for(int i=0; i<n; i++){
-         if(nums[i]==1){
+        }
+      }
+        for(int i=0; i<n; i++){
+        if(nums[i]==1){
             copy[j]=nums[i];
-             j++;
-         
-         }
-      }  
-       for(int i=0; i<n; i++){
-         if(nums[i]==2){
+            j++;
+        }
+      }
+        for(int i=0; i<n; i++){
+        if(nums[i]==2){
             copy[j]=nums[i];
-             j++;
-         
-         }
-      } 
-      for(int i=0; i<n; i++){
+            j++;
+        }
+      }
+        for(int i=0; i<n; i++){
         nums[i]=copy[i];
-      } 
+      }
     }
 }
