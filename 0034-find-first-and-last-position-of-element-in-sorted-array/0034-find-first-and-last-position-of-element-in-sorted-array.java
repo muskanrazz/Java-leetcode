787bbaa494f40d902f990a155b7c866f58fcbca1
Muskan kumari <muskanrazz006@gmail.com>
2026-05-24 22:54:57 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.31%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-       int  low = 0;
-       int  high = n-1;
-        int fi = -1;
-        
-        
+       int low =0;
+       int high= n-1;
+       int fi=-1;
+       int li= -1;
+
+       while(low<=high){
+        int mid = low + (high-low)/2;
+       
+        if(nums[mid]==target){
+        fi=mid;
+        high = mid - 1;
+        }
+        else if(nums[mid]>target) high =mid - 1;
+
+        else low = mid + 1;
+       }
+
+       low =0;
+       high =n-1;
+
         while(low<=high){
-            int mid = (low + high) /2;
-
-            if(nums[mid]==target){
-                fi = mid;
-            high = mid - 1;
-            }
-            else if(nums[mid]> target) high = mid - 1;
-
-            else{
-                low = mid +1;
-            }
+        int mid = low + (high-low)/2;
+      
+        if(nums[mid]==target){
+         li=mid;
+        low =mid + 1;
         }
 
-            int li = -1;
-             low = 0;
-             high = n-1;
-            
-              while(low<=high){
-            int mid = (low + high) /2;
+        else if(nums[mid]>target) high =mid - 1;
 
-            if(nums[mid]==target){
-                li = mid;
-            low = mid + 1;
-            }
-            else if(nums[mid]> target) high = mid - 1;
-
-            else  low = mid +1;
-
-          }
-        int arr[] = new int [2];
-        arr[0] = fi;
-        arr[1] = li;
-        return arr;
+        else low = mid + 1;
+       }
+       int arr[] = new int[2];
+       arr[0]= fi;
+       arr[1]= li;
+       return arr;
     }
 }
