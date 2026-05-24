37061b4588c9bfc4883bfaf6879ad06b1699247b
Muskan kumari <muskanrazz006@gmail.com>
2026-05-24 23:51:41 +0530

    Time: 5 ms (14.01%), Space: 48.3 MB (78.17%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,22 +3,26 @@ class Solution {
        // T.C= O(N)
        // S.C= O(1)
        int n =numbers.length;
-       int ans[] = new int [2];
-       int low=0;
+       int ans[]= new int[2];
+       for(int i=0; i<n; i++){
+       int low=i+1;
        int high =n-1;
 
        while(low<=high){
-        if(numbers[low] + numbers[high]==target){
-         ans[0]= low+1;
-         ans[1]=high+1;
+        int mid= low + (high-low)/2;
+        if(numbers[i]+numbers[mid]==target){
+         ans[0]= i+1;
+         ans[1]=mid+1;
          return ans;
         }
-        else if(numbers[low] + numbers[high]>target){
-            high--;
+        else if(numbers[i]+numbers[mid]>target){
+            high = mid - 1;
         }
         else{
-            low++;
+            low= mid + 1;
         }
+       }
+       
        }
        return ans;
     }
