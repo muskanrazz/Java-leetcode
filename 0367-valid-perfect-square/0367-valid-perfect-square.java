cb1b546b8435931c4d4fdddc619d2ee54e9f312f
Muskan kumari <muskanrazz006@gmail.com>
2026-05-24 22:31:48 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.35%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -5,9 +5,9 @@ class Solution {
 
        while(low<=high){
         long mid = low + (high-low)/2;
-        long ans = mid*mid;
-        if(ans==(long)num) return true;
-        else if(ans>(long)num) high = mid -1;
+  
+        if(mid*mid==(long)num) return true;
+        else if(mid*mid>(long)num) high = mid -1;
         else low = mid + 1;
        }
        return false;
