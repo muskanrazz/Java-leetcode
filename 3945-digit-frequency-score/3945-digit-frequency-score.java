0c314043d642099facbdef8200fc02a8655a6a1e
Muskan kumari <muskanrazz006@gmail.com>
2026-06-03 19:38:50 +0530

    Time: 3 ms (19.95%), Space: 42.9 MB (20.44%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,11 +1,21 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        int sum = 0;
+    
+    Map<Integer, Integer> map = new HashMap<>();
 
-        while(n!=0){
-            sum = sum + n%10;
-            n=n/10;
+    int sum =0;
+
+       while(n!=0){
+          int rem = n%10;
+          sum = sum+rem;
+        map.put(rem, map.getOrDefault(rem, 0)+1);
+         n= n/10;
+       }
+        for(int key: map.keySet()){
+        if(key * map.get(key)>0){
+        
         }
-        return sum;
+       }
+         return sum;
     }
 }
