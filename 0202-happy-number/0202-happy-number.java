a1020455e9a9b2bbf58984796cffb3e99bc7cf79
Muskan kumari <muskanrazz006@gmail.com>
2026-06-11 20:59:55 +0530

    Time: 0 ms (100%), Space: 42.1 MB (88.09%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,14 +3,13 @@ class Solution {
 
           if(n==1 || n==7) return true;
           if(n<10) return false;
-         
-            int sum =0;
-            while(n!=0){
-                int rem = n%10;
-                sum=sum+ rem*rem;
-            n=n/10;
-            } 
-       return isHappy(sum);
           
+          int sum=0;
+          while(n!=0){
+            int rem = n%10;
+           sum = sum+rem*rem;
+           n=n/10;
+          }
+          return isHappy(sum);
     }
 }
