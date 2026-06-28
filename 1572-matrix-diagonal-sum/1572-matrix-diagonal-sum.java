8d81e3be8319b092d9d97941b2d93146a57f8951
Muskan kumari <muskanrazz006@gmail.com>
2026-06-28 22:26:53 +0530

    Time: 0 ms (100%), Space: 46.2 MB (87.48%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,14 +1,15 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int n = mat.length;
-        int sum =0;
-        for(int i=0; i<n; i++){
-            sum= sum + mat[i][i];
-            sum = sum + mat[i] [n-i-1];
-        }
-        if(n%2!=0){
-            return sum-mat[n/2] [n/2];  // odd number me dubara middle element print na ho jaaye issliye 
-        }
-        return sum;
+       int n = mat.length;
+       int sum =0;
+
+       for(int i=0; i<n; i++){
+        sum = sum + mat[i] [i];
+        sum = sum + mat[i] [n-i-1];
+       }
+       if(n%2!=0){
+         return sum-mat[n/2] [n/2];
+       }
+       return sum;
     }
 }
