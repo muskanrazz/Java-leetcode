bf580ce25337eb5e19d90d3b47f5395e85e49afc
Muskan kumari <muskanrazz006@gmail.com>
2026-07-10 18:32:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,23 +1,21 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
        int n= people.length;
-       // T.C = NlogN + O(N)
-     Arrays.sort(people);  // sort dunc complexity is NlogN
-     int low =0;
-     int high = n-1;
-     int count=0;
+      int low=0;
+      int high = n-1;
+      int count=0;
 
-     while(low<=high){
+      while(low<=high){
         if(people[low] + people[high]<=limit){
             count++;
             low++;
             high--;
         }
-        else{
+        else {
             count++;
-            high--;
-        }
-     }
-        return count;
+            low++;
+      }
+      }
+      return count;
     }
 }
