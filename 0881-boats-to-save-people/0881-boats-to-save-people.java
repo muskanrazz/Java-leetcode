eed8588e37950da559040fd63ecf5290c263ca73
Muskan kumari <muskanrazz006@gmail.com>
2026-07-10 18:33:56 +0530

    Time: 20 ms (83.82%), Space: 56.7 MB (9.51%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -4,6 +4,7 @@ class Solution {
       int low=0;
       int high = n-1;
       int count=0;
+      Arrays.sort(people);
 
       while(low<=high){
         if(people[low] + people[high]<=limit){
@@ -13,7 +14,7 @@ class Solution {
         }
         else {
             count++;
-            low++;
+            high--;
       }
       }
       return count;
