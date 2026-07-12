c772a605f1e840fa4ca6ab74a7fd73b629a44975
Muskan kumari <muskanrazz006@gmail.com>
2026-07-12 16:44:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (19.16%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-       long low =1;
-       long high = num;
+     long low=1;
+     long high= num;
 
-       while(low<=high){
-        long mid = low + (high-low)/2;
-  
+     while(low<=high){
+        long mid= low + (high-low)/2;
         if(mid*mid==(long)num) return true;
-        else if(mid*mid>(long)num) high = mid -1;
+        else if(mid*mid>(long)num) high = mid - 1;
         else low = mid + 1;
-       }
-       return false;
-       
-    }
+     }
+     return false;
+     
+   }
 }
