376c5f56ee6e7f6b751400b0b1ca01773d080452
Muskan kumari <muskanrazz006@gmail.com>
2026-07-15 21:48:29 +0530

    Time: 4 ms (100%), Space: 46.1 MB (17.22%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,21 +1,13 @@
 class Solution {
+       public static int reverse(int rev,int n){
+        if(n<=0) return rev;
+        return reverse(rev*10+n%10,n/10);
+    }
     public boolean isPalindrome(int x) {
-        int rev=0;
-        int y = x;
-        if(x<0){
-            return false;
-        
-        }
-        while(x!=0){
-            int rem = x%10;
-            rev= rev*10+rem;
-            x=x/10;
-        }
-        if(rev==y){
-            return true;
-        }
-        else{
-            return false;
-        }
+       int rev=0;
+    //    reverse(rev,x);
+       if(reverse(rev,x)==x) return true;
+       else return false;
+
     }
 }
